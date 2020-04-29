c6cdd3d798286e077af850c113b01334ee8a96bf
mariklolik <marshelo44@gmail.com>
2020-04-30 01:53:46 +0700

    Git init

---
+++ study.py
@@ -0,0 +1,15 @@
+import logging
+logging.basicConfig(
+    filename='example.log',
+    format='%(asctime)s %(levelname)s %(name)s %(message)s'
+)
+
+def log():
+    i = 0
+    while i < 10:
+        logging.warning(i)
+        i += 1
+
+
+if __name__ == '__main__':
+    log()
